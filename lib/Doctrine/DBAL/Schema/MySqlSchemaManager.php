0ed2e1d536da9d0c8f212148657d0eabbc8cfad4
ryunosuke <ryunosuke.arima@gmail.com>
2015-01-27 21:00:00 +0900

    add FIRST/AFTER

--- lib/Doctrine/DBAL/Schema/MySqlSchemaManager.php
+++ lib/Doctrine/DBAL/Schema/MySqlSchemaManager.php
@@ -216,6 +216,10 @@ class MySqlSchemaManager extends AbstractSchemaManager
             $column->setPlatformOption('collation', $tableColumn['collation']);
         }
 
+        if (isset($tableColumn['beforecolumn'])) {
+            $column->setPlatformOption('beforeColumn', $tableColumn['beforecolumn']);
+        }
+
         return $column;
     }
 
