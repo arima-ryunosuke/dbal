be121f84c184f85a0d28515218e2f5e5bbb7b687
ryunosuke <ryunosuke.arima@gmail.com>
2018-12-25 19:44:20 +0900

    add Spatial Types

---
+++ lib/Doctrine/DBAL/Types/MySql/SpatialType.php
@@ -0,0 +1,64 @@
+<?php
+namespace Doctrine\DBAL\Types\MySql;
+
+use Doctrine\DBAL\Platforms\AbstractPlatform;
+use Doctrine\DBAL\Types\Type;
+
+abstract class SpatialType extends Type
+{
+    /**
+     * @return Type[]
+     */
+    public static function addSpatialTypes()
+    {
+        $addedTypes = [];
+
+        $files = scandir(__DIR__);
+        foreach ($files as $file) {
+            if ($file[0] === '.') {
+                continue;
+            }
+            if ($file === basename(__FILE__)) {
+                continue;
+            }
+
+            $basename = basename($file, '.php');
+            $name = preg_replace('/Type$/', '', $basename);
+            $typeName = strtolower($name);
+            $className = __NAMESPACE__ . '\\' . $basename;
+
+            if (! Type::hasType($typeName)) {
+                Type::addType($typeName, $className);
+                $addedTypes[$typeName] = Type::getType($typeName);
+            }
+        }
+
+        return $addedTypes;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        $parts = explode('\\', get_class($this));
+        $name = end($parts);
+        return strtolower(preg_replace('/Type$/', '', $name));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getSQLDeclaration(array $fieldDeclaration, AbstractPlatform $platform)
+    {
+        return strtoupper($this->getName());
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function convertToPHPValue($value, AbstractPlatform $platform)
+    {
+        return (null === $value) ? null : $value;
+    }
+}
