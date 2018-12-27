343351ba406a49a34ba8f7c02b45456a22dbd4a7
ryunosuke <ryunosuke.arima@gmail.com>
2018-12-27 22:39:55 +0900

    fix diffColumn in text length is different
    - Because mysql has multiple TEXT types.

--- tests/Doctrine/Tests/DBAL/Schema/ComparatorTest.php
+++ tests/Doctrine/Tests/DBAL/Schema/ComparatorTest.php
@@ -228,6 +228,18 @@ class ComparatorTest extends TestCase
         self::assertEquals([], $c->diffColumn($column1, $column2));
     }
 
+    public function testCompareChangedColumns_ChangeTextLength(): void
+    {
+        $column1 = new Column('textfield1', Type::getType('text'), ['Length' => 128]);
+        $column2 = new Column('textfield1', Type::getType('text'), ['Length' => 65536]);
+        $column3 = new Column('textfield1', Type::getType('text'), ['Length' => 65536]);
+
+        $c = new Comparator();
+        self::assertEquals(['length'], $c->diffColumn($column1, $column2));
+        self::assertEquals([], $c->diffColumn($column1, $column1));
+        self::assertEquals([], $c->diffColumn($column2, $column3));
+    }
+
     public function testCompareChangedColumnsChangeCustomSchemaOption(): void
     {
         $column1 = new Column('charcolumn1', Type::getType('string'));
