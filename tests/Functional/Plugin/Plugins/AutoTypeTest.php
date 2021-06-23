cdfe2cf04b2aaae9c6c8a3ce58d18cfd479d9344
ryunosuke <ryunosuke.arima@gmail.com>
2021-06-23 23:59:48 +0900

    add tests

---
+++ tests/Functional/Plugin/Plugins/AutoTypeTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Doctrine\DBAL\Tests\Functional\Plugin\Plugins;
+
+use Doctrine\DBAL\Types\Type;
+
+class AutoTypeTest extends AbstractPluginTestCase
+{
+    protected bool $autoskip = false;
+
+    public function testAutoType(): void
+    {
+        $platform = $this->connection->getDatabasePlatform();
+        $platform->enableAutoType(true);
+
+        $name = $platform->getDoctrineTypeMapping('hoge');
+        self::assertEquals('hoge', $name);
+
+        $type = Type::getType('hoge');
+        self::assertEquals('hoge', $type->getName());
+        self::assertEquals('hoge', Type::getTypeRegistry()->lookupName($type));
+        self::assertEquals('HOGE(misc)', $type->getSQLDeclaration(['type-declaration' => '(misc)'], $platform));
+
+        $platform->enableAutoType(false);
+    }
+}
