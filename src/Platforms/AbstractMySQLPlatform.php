0eb2ecc031214cf9174322a21100524dc486d82d
ryunosuke <ryunosuke.arima@gmail.com>
2022-04-15 20:47:37 +0900

    [mysql] support DATETIME/TIME fraction

--- src/Platforms/AbstractMySQLPlatform.php
+++ src/Platforms/AbstractMySQLPlatform.php
@@ -166,11 +166,16 @@ abstract class AbstractMySQLPlatform extends AbstractPlatform
      */
     public function getDateTimeTypeDeclarationSQL(array $column): string
     {
-        if (isset($column['version']) && $column['version'] === true) {
-            return 'TIMESTAMP';
+        $length = '';
+        if (isset($column['length']) && $column['length']) {
+            $length = '(' . $column['length'] . ')';
         }
 
-        return 'DATETIME';
+        if (isset($column['version']) && $column['version'] === true) {
+            return 'TIMESTAMP' . $length;
+        }
+
+        return 'DATETIME' . $length;
     }
 
     /**
@@ -186,7 +191,12 @@ abstract class AbstractMySQLPlatform extends AbstractPlatform
      */
     public function getTimeTypeDeclarationSQL(array $column): string
     {
-        return 'TIME';
+        $length = '';
+        if (isset($column['length']) && $column['length']) {
+            $length = '(' . $column['length'] . ')';
+        }
+
+        return 'TIME' . $length;
     }
 
     /**
