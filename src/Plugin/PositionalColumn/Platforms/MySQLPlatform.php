c503409f881a278bed91a57cf02b8b070ee28e5f
ryunosuke <ryunosuke.arima@gmail.com>
2022-11-27 00:52:40 +0900

    add PositionalColumn plugin

---
+++ src/Plugin/PositionalColumn/Platforms/MySQLPlatform.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Doctrine\DBAL\Plugin\PositionalColumn\Platforms;
+
+use Doctrine\DBAL\Schema\Column;
+use Doctrine\DBAL\Schema\Table;
+
+trait MySQLPlatform
+{
+    /**
+     * @used-by \Doctrine\DBAL\Platforms\MySQLPlatform::getColumnDeclarationSQL()
+     */
+    protected function interceptGetColumnDeclarationSQLByPositionalColumn(string $declaration, array $column, string $name): array
+    {
+        // empty name is compare context
+        if ($name === '') {
+            return [];
+        }
+
+        if (array_key_exists('beforeColumn', $column)) {
+            if ($column['beforeColumn']) {
+                $declaration .= " AFTER " . $column['beforeColumn'];
+            } else {
+                $declaration .= " FIRST";
+            }
+        }
+
+        return ['declaration' => $declaration];
+    }
+
+    public function getColumnDeclarationListSQL(array $columns): string
+    {
+        // premise: this method is called when create table context only
+
+        foreach ($columns as &$column) {
+            unset($column['beforeColumn']);
+        }
+
+        return parent::getColumnDeclarationListSQL($columns);
+    }
+}
