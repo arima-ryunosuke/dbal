af34e161b402158731c21f5f3d29c9325ed9cd35
ryunosuke <ryunosuke.arima@gmail.com>
2022-11-27 07:33:25 +0900

    add Event plugin

---
+++ src/Plugin/Event/Schema/Comparator.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Doctrine\DBAL\Plugin\Event\Schema;
+
+use Doctrine\DBAL\Schema\Event;
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\DBAL\Schema\SchemaDiff;
+
+trait Comparator
+{
+    /**
+     * @used-by \Doctrine\DBAL\Schema\Comparator::compareSchemas()
+     */
+    protected function interceptCompareSchemasByEvent(Schema $oldSchema, Schema $newSchema, SchemaDiff $diff): array
+    {
+        foreach ($oldSchema->getEvents() as $event) {
+            if ($newSchema->hasEvent($event->getLocalName())) {
+                continue;
+            }
+
+            $diff->droppedEvents[] = $event;
+        }
+
+        foreach ($newSchema->getEvents() as $event) {
+            if (! $oldSchema->hasEvent($event->getLocalName())) {
+                $diff->createdEvents[] = $event;
+            } elseif ($diffs = $this->diffEvent($oldSchema->getEvent($event->getLocalName()), $event)) {
+                $diff->alteredEvents[] = $event;
+                $diff->eventDiffs[]    = $diffs;
+            }
+        }
+
+        return [];
+    }
+
+    public function diffEvent(Event $event1, Event $event2): array
+    {
+        $diff = [];
+
+        if (trim($event1->getStatement()) !== trim($event2->getStatement())) {
+            $diff[0]['statement'] = $event1->getStatement();
+            $diff[1]['statement'] = $event2->getStatement();
+        }
+
+        $options1 = $event1->getOptions();
+        $options2 = $event2->getOptions();
+
+        // timezone is session value and not present in create/alter SQL
+        unset($options1['timeZone']);
+        unset($options2['timeZone']);
+
+        // interval is generated value by intervalField/intervalValue
+        unset($options1['interval']);
+        unset($options2['interval']);
+
+        if ($options1 != $options2) {
+            $diff[0]['options'] = $options1;
+            $diff[1]['options'] = $options2;
+        }
+
+        return $diff;
+    }
+}
---
+++ src/Plugin/Event/Schema/Schema.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Doctrine\DBAL\Plugin\Event\Schema;
+
+use Doctrine\DBAL\Plugin\Event\Schema\Exception\EventAlreadyExists;
+use Doctrine\DBAL\Plugin\Event\Schema\Exception\EventDoesNotExist;
+use Doctrine\DBAL\Schema\Event;
+
+trait Schema
+{
+    /** @var Event[] */
+    protected array $_events = [];
+
+    /**
+     * @used-by \Doctrine\DBAL\Schema\Schema::__clone()
+     */
+    protected function intercept__CloneByEvent(): array
+    {
+        foreach ($this->_events as $k => $event) {
+            $this->_events[$k] = clone $event;
+        }
+
+        return [];
+    }
+
+    /** @return Event[] */
+    public function getEvents(): array
+    {
+        return $this->_events;
+    }
+
+    public function getEvent(string $eventName): Event
+    {
+        $name = $this->getKeyFromName($eventName);
+        if (! isset($this->_events[$name])) {
+            throw EventDoesNotExist::new($name);
+        }
+
+        return $this->_events[$name];
+    }
+
+    public function hasEvent(string $eventName): bool
+    {
+        $name = $this->getKeyFromName($eventName);
+
+        return isset($this->_events[$name]);
+    }
+
+    public function addEvent(Event $event): self
+    {
+        $eventName = $this->getKeyFromResolvedName($event);
+
+        if (isset($this->_events[$eventName])) {
+            throw EventAlreadyExists::new($eventName);
+        }
+
+        $this->_events[$eventName] = $event;
+
+        $this->_createNamespaceBy($event);
+
+        return $this;
+    }
+
+    public function dropEvent(string $eventName): self
+    {
+        $name = $this->getKeyFromName($eventName);
+        if (! isset($this->_events[$name])) {
+            throw EventDoesNotExist::new($name);
+        }
+
+        unset($this->_events[$name]);
+
+        return $this;
+    }
+}
