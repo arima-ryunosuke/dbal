34a36ed38ba67d91c4c2861cfbfe0b376a962051
ryunosuke <ryunosuke.arima@gmail.com>
2022-11-27 01:04:08 +0900

    [mysql] fix Useless comment space

--- src/Platforms/AbstractMySQLPlatform.php
+++ src/Platforms/AbstractMySQLPlatform.php
@@ -345,7 +345,7 @@ abstract class AbstractMySQLPlatform extends AbstractPlatform
 
         // Comment
         if (isset($options['comment'])) {
-            $tableOptions[] = sprintf('COMMENT = %s ', $this->quoteStringLiteral($options['comment']));
+            $tableOptions[] = sprintf('COMMENT = %s', $this->quoteStringLiteral($options['comment']));
         }
 
         // Row format
