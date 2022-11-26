294343ebf36c230bcab965c754cbee15b2a117d8
ryunosuke <ryunosuke.arima@gmail.com>
2022-11-27 00:51:31 +0900

    add View plugin

---
+++ src/Plugin/View/Platforms/MySQLPlatform.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Doctrine\DBAL\Plugin\View\Platforms;
+
+use Doctrine\DBAL\Schema\View;
+
+trait MySQLPlatform
+{
+    /**
+     * @used-by \Doctrine\DBAL\Platforms\MySQLPlatform::getCreateViewSQL()
+     */
+    protected function interceptGetCreateViewSQLByView(string $query, array $options): array
+    {
+        $checkOption = $this->_getCheckOption($options);
+        if ($checkOption !== null) {
+            return ['query' => "$query $checkOption"];
+        }
+
+        return [];
+    }
+
+    public function getAlterViewSQL(View $view): array
+    {
+        $query = 'ALTER VIEW ' . $view->getName() . ' AS ' . $view->getSql();
+
+        $checkOption = $this->_getCheckOption($view->getOptions());
+        if ($checkOption !== null) {
+            $query .= ' ' . $checkOption;
+        }
+
+        return [$query];
+    }
+
+    private function _getCheckOption(array $options): ?string
+    {
+        $checkOption = strtoupper($options['checkOption'] ?? 'NONE');
+        if ($checkOption !== 'NONE') {
+            return "WITH $checkOption CHECK OPTION";
+        }
+
+        return null;
+    }
+}
