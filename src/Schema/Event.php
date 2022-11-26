45cdb7b96278f2ab2fefef98c22ad485427817d7
ryunosuke <ryunosuke.arima@gmail.com>
2022-11-27 07:33:25 +0900

    add Event plugin

---
+++ src/Schema/Event.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace Doctrine\DBAL\Schema;
+
+use Doctrine\DBAL\Schema\Name\Parser\OptionallyQualifiedNameParser;
+use Doctrine\DBAL\Schema\Name\Parsers;
+
+/**
+ * Representation of a Database Event.
+ */
+class Event extends AbstractNamedObject
+{
+    /** @var string */
+    private $_statement;
+
+    /** @var array */
+    protected $_options = [];
+
+    /**
+     * @param string $name
+     * @param string $statement
+     * @param mixed[] $options
+     */
+    public function __construct($name, $statement, array $options = [])
+    {
+        parent::__construct($name);
+
+        $this->_statement = $statement;
+
+        $this->_options = $options;
+    }
+
+    protected function getNameParser(): OptionallyQualifiedNameParser
+    {
+        return Parsers::getOptionallyQualifiedNameParser();
+    }
+
+    /**
+     * @return string
+     */
+    public function getStatement()
+    {
+        return $this->_statement;
+    }
+
+    /**
+     * @param string $name
+     * @param string $value
+     *
+     * @return self
+     */
+    public function addOption($name, $value)
+    {
+        $this->_options[$name] = $value;
+
+        return $this;
+    }
+
+    /**
+     * @param string $name
+     *
+     * @return bool
+     */
+    public function hasOption($name)
+    {
+        return isset($this->_options[$name]);
+    }
+
+    /**
+     * @param string $name
+     *
+     * @return mixed
+     */
+    public function getOption($name)
+    {
+        return $this->_options[$name] ?? null;
+    }
+
+    /**
+     * @return array
+     */
+    public function getOptions()
+    {
+        return $this->_options;
+    }
+}
