cbe984a3262d01f187f21aed27353933514cc591
ryunosuke <ryunosuke.arima@gmail.com>
2022-11-27 00:52:19 +0900

    add GeneratedColumn plugin

---
+++ src/Plugin/GeneratedColumn/Schema/MySQLSchemaManager.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Doctrine\DBAL\Plugin\GeneratedColumn\Schema;
+
+use Doctrine\DBAL\Schema\Column;
+
+trait MySQLSchemaManager
+{
+    /**
+     * @used-by \Doctrine\DBAL\Schema\MySQLSchemaManager::selectTableColumns()
+     */
+    protected function interceptSelectTableColumnsByGeneratedColumn(string $sql): array
+    {
+        $sql = strtr($sql, [
+            'FROM information_schema' => ",c.GENERATION_EXPRESSION AS generationexpression\nFROM information_schema",
+        ]);
+
+        return ['sql' => $sql];
+    }
+
+    /**
+     * @used-by \Doctrine\DBAL\Schema\MySQLSchemaManager::_getPortableTableColumnDefinition()
+     */
+    protected function intercept_GetPortableTableColumnDefinitionByGeneratedColumn(array $tableColumn, Column $column): array
+    {
+        // no check "extra" key. because it contains other "GENERATED" keyword. e.g. "DEFAULT_GENERATED" on DATETIME
+        if (strlen($tableColumn['generationexpression'] ?? '')) {
+            $column->setPlatformOption('generation', [
+                'type'       => strstr($tableColumn['extra'], ' ', true),
+                'expression' => stripslashes($tableColumn['generationexpression']),
+            ]);
+        }
+
+        return [];
+    }
+}
