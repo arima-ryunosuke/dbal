a2978595bd91273a897b890753e04a66445f21ad
ryunosuke <ryunosuke.arima@gmail.com>
2022-11-27 07:33:25 +0900

    add Event plugin

---
+++ src/Plugin/Event/Platforms/AbstractPlatform.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Doctrine\DBAL\Plugin\Event\Platforms;
+
+use Doctrine\DBAL\Platforms\Exception\NotSupported;
+use Doctrine\DBAL\Schema\Event;
+use Doctrine\DBAL\Schema\SchemaDiff;
+
+/**
+ * @codeCoverageIgnore
+ */
+trait AbstractPlatform
+{
+    /**
+     * @used-by \Doctrine\DBAL\Platforms\MySQLPlatform::getAlterSchemaSQL()
+     */
+    protected function interceptGetAlterSchemaSQLByEvent(array $sql, SchemaDiff $diff): array
+    {
+        foreach ($diff->createdEvents as $event) {
+            $sql[] = $this->getCreateEventSQL($event);
+        }
+
+        foreach ($diff->droppedEvents as $event) {
+            $sql[] = $this->getDropEventSQL($event->getLocalName());
+        }
+
+        foreach ($diff->alteredEvents as $n => $event) {
+            $alteredSqls       = $this->getAlterEventSQL($event);
+            $key               = array_key_first($alteredSqls);
+            $alteredSqls[$key] = $this->buildDiffComment($diff->eventDiffs[$n]) . $alteredSqls[$key];
+
+            $sql = array_merge($sql, $alteredSqls);
+        }
+
+        return ['sql' => $sql];
+    }
+
+    public function supportsEvents(): bool
+    {
+        return false;
+    }
+
+    public function getCreateEventSQL(Event $event): string
+    {
+        throw NotSupported::new(__METHOD__);
+    }
+
+    public function getDropEventSQL(string $event): string
+    {
+        throw NotSupported::new(__METHOD__);
+    }
+
+    public function getAlterEventSQL(Event $event): array
+    {
+        return [
+            $this->getDropEventSQL($event->getLocalName()),
+            $this->getCreateEventSQL($event),
+        ];
+    }
+}
