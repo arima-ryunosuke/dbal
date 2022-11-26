e62af6671b88a0450e388f770207ddc03eba7a9a
ryunosuke <ryunosuke.arima@gmail.com>
2022-11-27 00:53:02 +0900

    add Trigger plugin

---
+++ src/Plugin/Trigger/Platforms/PostgreSQLPlatform.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Doctrine\DBAL\Plugin\Trigger\Platforms;
+
+use Doctrine\DBAL\Schema\Trigger;
+
+/**
+ * @codeCoverageIgnore this is implemented to broaden one's horizons
+ */
+trait PostgreSQLPlatform
+{
+    public function supportsTriggers(): bool
+    {
+        return true;
+    }
+
+    public function getCreateTriggerSQL(Trigger $trigger): string
+    {
+        $triggerName = $trigger->getQuotedName($this);
+        $tableName   = $trigger->getTableName();
+        $statement   = $trigger->getStatement();
+        $options     = $trigger->getOptions();
+        $timing      = $options['timing'] ?? null;
+        $event       = $options['event'] ?? null;
+        $foreach     = $options['orientation'] ?? 'ROW';
+
+        return "CREATE TRIGGER $triggerName $timing $event ON $tableName FOR EACH $foreach $statement";
+    }
+
+    public function getDropTriggerSQL(string $trigger, string $table = ''): string
+    {
+        // IF EXISTS is required because Trigger dropped with drop table collateral damage
+        return "DROP TRIGGER IF EXISTS $trigger ON $table";
+    }
+}
