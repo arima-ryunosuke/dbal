a2dbff0c9a9e313b56fc1d5517b4247f6397e90e
ryunosuke <ryunosuke.arima@gmail.com>
2022-11-27 07:33:25 +0900

    add Event plugin

---
+++ src/Plugin/Event/Platforms/MySQLPlatform.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Doctrine\DBAL\Plugin\Event\Platforms;
+
+use Doctrine\DBAL\Schema\Event;
+
+trait MySQLPlatform
+{
+    public function supportsEvents(): bool
+    {
+        return true;
+    }
+
+    public function getCreateEventSQL(Event $event): string
+    {
+        return 'CREATE ' . $this->_getEventSQL($event);
+    }
+
+    public function getDropEventSQL(string $event): string
+    {
+        return "DROP EVENT $event";
+    }
+
+    public function getAlterEventSQL(Event $event): array
+    {
+        return ['ALTER ' . $this->_getEventSQL($event)];
+    }
+
+    private function _getEventSQL(Event $event): string
+    {
+        $eventName = $event->getQuotedName($this);
+        $statement = $event->getStatement();
+        $options   = $event->getOptions();
+        $definer   = $this->_quoteUserHostname($options['definer'] ?? '');
+
+        return implode("\n", array_filter([
+            ($definer ? "DEFINER=$definer " : "") . "EVENT $eventName",
+            "ON SCHEDULE EVERY {$this->quoteStringLiteral($options['intervalValue'])} {$options['intervalField']}",
+            $options['since'] ? "STARTS {$this->quoteStringLiteral($options['since'])}" : '',
+            $options['until'] ? "ENDS {$this->quoteStringLiteral($options['until'])}" : '',
+            "ON COMPLETION {$options['completion']}",
+            "{$options['status']}",
+            "COMMENT {$this->quoteStringLiteral($options['comment'])}",
+            "DO $statement",
+        ], 'strlen'));
+    }
+}
