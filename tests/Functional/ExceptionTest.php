2c4d2aca07a42da717a3cb6ff339fde824f93165
ryunosuke <ryunosuke.arima@gmail.com>
2023-02-05 03:07:22 +0900

    [sqlite] fix windows test

--- tests/Functional/ExceptionTest.php
+++ tests/Functional/ExceptionTest.php
@@ -293,6 +293,7 @@ class ExceptionTest extends FunctionalTestCase
                 $conn->executeStatement($sql);
             }
         } finally {
+            $conn->close();
             $this->cleanupReadOnlyFile($filename);
         }
     }
