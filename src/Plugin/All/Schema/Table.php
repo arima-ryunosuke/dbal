2f3c8e01b709e1afe64f5ac516c875a96684e9fe
ryunosuke <ryunosuke.arima@gmail.com>
2023-09-21 18:18:03 +0900

    build all plugin

---
+++ src/Plugin/All/Schema/Table.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Doctrine\DBAL\Plugin\All\Schema;
+
+/**
+ * @used-by \Doctrine\DBAL\Schema\Table
+ */
+trait Table
+{
+    // @formatter:off,auto-generated:begin
+    use \Doctrine\DBAL\Plugin\ImplicitIndex\Schema\Table;
+    use \Doctrine\DBAL\Plugin\PositionalColumn\Schema\Table;
+    // @formatter:on,auto-generated:end
+}
