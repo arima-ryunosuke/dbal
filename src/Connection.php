d1eeaca27c41726b0e67b838c03a48ef3663b28e
ryunosuke <ryunosuke.arima@gmail.com>
2023-09-21 17:09:26 +0900

    [Connection] add prepare parameters(params, types)

--- src/Connection.php
+++ src/Connection.php
@@ -793,7 +793,7 @@ class Connection implements ServerVersionProvider
             if (count($params) > 0) {
                 [$sql, $params, $types] = $this->expandArrayParameters($sql, $params, $types);
 
-                $stmt = $connection->prepare($sql);
+                $stmt = $connection->prepare($sql, $params, $types);
 
                 $this->bindParameters($stmt, $params, $types);
 
@@ -896,7 +896,7 @@ class Connection implements ServerVersionProvider
             if (count($params) > 0) {
                 [$sql, $params, $types] = $this->expandArrayParameters($sql, $params, $types);
 
-                $stmt = $connection->prepare($sql);
+                $stmt = $connection->prepare($sql, $params, $types);
 
                 $this->bindParameters($stmt, $params, $types);
 
