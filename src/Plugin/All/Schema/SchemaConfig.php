9c063f034053f02586c7b28748b1bcfbf31fdbc5
ryunosuke <ryunosuke.arima@gmail.com>
2023-09-21 18:18:03 +0900

    build all plugin

---
+++ src/Plugin/All/Schema/SchemaConfig.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Doctrine\DBAL\Plugin\All\Schema;
+
+/**
+ * @used-by \Doctrine\DBAL\Schema\SchemaConfig
+ */
+trait SchemaConfig
+{
+    // @formatter:off,auto-generated:begin
+    use \Doctrine\DBAL\Plugin\PositionalColumn\Schema\SchemaConfig;
+    // @formatter:on,auto-generated:end
+}
