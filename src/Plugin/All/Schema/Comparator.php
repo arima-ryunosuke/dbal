a40e2c5476810c54fb3c3b469c09fc5dbedabb90
ryunosuke <ryunosuke.arima@gmail.com>
2023-09-21 18:18:03 +0900

    build all plugin

---
+++ src/Plugin/All/Schema/Comparator.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Doctrine\DBAL\Plugin\All\Schema;
+
+/**
+ * @used-by \Doctrine\DBAL\Schema\Comparator
+ */
+trait Comparator
+{
+    // @formatter:off,auto-generated:begin
+    use \Doctrine\DBAL\Plugin\Event\Schema\Comparator;
+    use \Doctrine\DBAL\Plugin\IndexOption\Schema\Comparator;
+    use \Doctrine\DBAL\Plugin\PositionalColumn\Schema\Comparator;
+    use \Doctrine\DBAL\Plugin\Routine\Schema\Comparator;
+    use \Doctrine\DBAL\Plugin\TableOption\Schema\Comparator;
+    use \Doctrine\DBAL\Plugin\Trigger\Schema\Comparator;
+    use \Doctrine\DBAL\Plugin\View\Schema\Comparator;
+    // @formatter:on,auto-generated:end
+}
---
+++ src/Plugin/All/Schema/PostgreSQLSchemaManager.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Doctrine\DBAL\Plugin\All\Schema;
+
+/**
+ * @used-by \Doctrine\DBAL\Schema\PostgreSQLSchemaManager
+ */
+trait PostgreSQLSchemaManager
+{
+    // @formatter:off,auto-generated:begin
+    use \Doctrine\DBAL\Plugin\Routine\Schema\PostgreSQLSchemaManager;
+    use \Doctrine\DBAL\Plugin\Trigger\Schema\PostgreSQLSchemaManager;
+    use \Doctrine\DBAL\Plugin\View\Schema\PostgreSQLSchemaManager;
+    // @formatter:on,auto-generated:end
+}
