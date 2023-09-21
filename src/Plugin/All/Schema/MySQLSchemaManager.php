4d1c0576cd26ff6dac7199729fa3da16382cf24b
ryunosuke <ryunosuke.arima@gmail.com>
2023-09-21 18:18:03 +0900

    build all plugin

---
+++ src/Plugin/All/Schema/MySQLSchemaManager.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Doctrine\DBAL\Plugin\All\Schema;
+
+/**
+ * @used-by \Doctrine\DBAL\Schema\MySQLSchemaManager
+ */
+trait MySQLSchemaManager
+{
+    // @formatter:off,auto-generated:begin
+    use \Doctrine\DBAL\Plugin\AutoType\Schema\MySQLSchemaManager;
+    use \Doctrine\DBAL\Plugin\Event\Schema\MySQLSchemaManager;
+    use \Doctrine\DBAL\Plugin\ExpressionDefaultColumn\Schema\MySQLSchemaManager;
+    use \Doctrine\DBAL\Plugin\GeneratedColumn\Schema\MySQLSchemaManager;
+    use \Doctrine\DBAL\Plugin\IndexOption\Schema\MySQLSchemaManager;
+    use \Doctrine\DBAL\Plugin\PositionalColumn\Schema\MySQLSchemaManager;
+    use \Doctrine\DBAL\Plugin\Routine\Schema\MySQLSchemaManager;
+    use \Doctrine\DBAL\Plugin\TableOption\Schema\MySQLSchemaManager;
+    use \Doctrine\DBAL\Plugin\Trigger\Schema\MySQLSchemaManager;
+    use \Doctrine\DBAL\Plugin\ViewOption\Schema\MySQLSchemaManager;
+    // @formatter:on,auto-generated:end
+}
---
+++ src/Plugin/All/Schema/View.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Doctrine\DBAL\Plugin\All\Schema;
+
+/**
+ * @used-by \Doctrine\DBAL\Schema\View
+ */
+trait View
+{
+    // @formatter:off,auto-generated:begin
+    use \Doctrine\DBAL\Plugin\ViewOption\Schema\View;
+    // @formatter:on,auto-generated:end
+}
