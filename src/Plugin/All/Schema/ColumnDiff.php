0829468b8311ce7bd4a0cdc24c56ef8ad1f87b73
ryunosuke <ryunosuke.arima@gmail.com>
2023-09-21 18:18:03 +0900

    build all plugin

---
+++ src/Plugin/All/Schema/ColumnDiff.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Doctrine\DBAL\Plugin\All\Schema;
+
+/**
+ * @used-by \Doctrine\DBAL\Schema\ColumnDiff
+ */
+trait ColumnDiff
+{
+    // @formatter:off,auto-generated:begin
+    use \Doctrine\DBAL\Plugin\DiffComment\Schema\ColumnDiff;
+    // @formatter:on,auto-generated:end
+}
---
+++ src/Plugin/All/Schema/Index.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Doctrine\DBAL\Plugin\All\Schema;
+
+/**
+ * @used-by \Doctrine\DBAL\Schema\Index
+ */
+trait Index
+{
+    // @formatter:off,auto-generated:begin
+    use \Doctrine\DBAL\Plugin\IndexOption\Schema\Index;
+    // @formatter:on,auto-generated:end
+}
