b88b368cbc7135cfefece4b24bda2debcf27a0be
ryunosuke <ryunosuke.arima@gmail.com>
2023-09-21 18:18:03 +0900

    build all plugin

---
+++ src/Plugin/All/Schema/SchemaDiff.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Doctrine\DBAL\Plugin\All\Schema;
+
+/**
+ * @used-by \Doctrine\DBAL\Schema\SchemaDiff
+ */
+trait SchemaDiff
+{
+    // @formatter:off,auto-generated:begin
+    use \Doctrine\DBAL\Plugin\Event\Schema\SchemaDiff;
+    use \Doctrine\DBAL\Plugin\Routine\Schema\SchemaDiff;
+    use \Doctrine\DBAL\Plugin\Trigger\Schema\SchemaDiff;
+    use \Doctrine\DBAL\Plugin\View\Schema\SchemaDiff;
+    // @formatter:on,auto-generated:end
+}
---
+++ src/Plugin/All/Schema/Table.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Doctrine\DBAL\Plugin\All\Schema;
+
+/**
+ * @used-by \Doctrine\DBAL\Schema\Table
+ */
+trait Table
+{
+    // @formatter:off,auto-generated:begin
+    use \Doctrine\DBAL\Plugin\IndexOption\Schema\Table;
+    // @formatter:on,auto-generated:end
+}
