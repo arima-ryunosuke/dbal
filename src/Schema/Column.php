35fdb2f122332cd93b83b57b91078c3535a69c11
ryunosuke <ryunosuke.arima@gmail.com>
2024-03-05 17:02:45 +0900

    [Schema] fix getOption returns default null

--- src/Schema/Column.php
+++ src/Schema/Column.php
@@ -320,7 +320,7 @@ class Column extends AbstractNamedObject
     public function getPlatformOption(string $name): mixed
     {
         /** @phpstan-ignore offsetAccess.notFound */
-        return $this->_platformOptions[$name];
+        return $this->_platformOptions[$name] ?? null;
     }
 
     public function getColumnDefinition(): ?string
