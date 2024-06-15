0b0d929e6f709ad2d0f0179f89086e6d7c4fa968
ryunosuke <ryunosuke.arima@gmail.com>
2024-06-15 18:38:17 +0900

    add AutoType plugin

---
+++ src/Plugin/AutoType/Schema/MySQLSchemaManager.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Doctrine\DBAL\Plugin\AutoType\Schema;
+
+use Doctrine\DBAL\Schema\Column;
+use Doctrine\DBAL\Types\AnyType;
+
+trait MySQLSchemaManager
+{
+    /**
+     * @used-by \Doctrine\DBAL\Schema\MySQLSchemaManager::_getPortableTableColumnDefinition()
+     */
+    protected function intercept_GetPortableTableColumnDefinitionByAutoType(array $tableColumn, Column $column): array
+    {
+        if ($column->getType() instanceof AnyType) {
+            $pos = strpos($tableColumn['type'], '(');
+            if ($pos !== false) {
+                $column->setPlatformOption('type-declaration', substr($tableColumn['type'], $pos));
+            }
+        }
+
+        return [];
+    }
+}
