d13ba484a9978afd84b7e8f7ef69cdc582e95c90
ryunosuke <ryunosuke.arima@gmail.com>
2024-08-15 23:36:19 +0900

    [Driver] add common parameter "timeout"

--- tests/Driver/AbstractSQLServerDriverTestCase.php
+++ tests/Driver/AbstractSQLServerDriverTestCase.php
@@ -44,4 +44,23 @@ abstract class AbstractSQLServerDriverTestCase extends AbstractDriverTestCase
     {
         self::markTestSkipped('SQL Server drivers do not use server version to instantiate platform');
     }
+
+    public function testTimeoutParameter(): void
+    {
+        if (!extension_loaded('sqlsrv') || $this->driver instanceof \Doctrine\DBAL\Driver\PDO\SQLSrv\Driver) {
+            self::markTestSkipped();
+        }
+
+        $time = microtime(true);
+        try {
+            $this->driver->connect([
+                'host'    => '192.0.2.0',
+                'timeout' => 2,
+            ]);
+            self::fail();
+        } catch (\Exception $exception) {
+            self::assertEquals(53, $exception->getCode());
+            self::assertLessThan(2.5, microtime(true) - $time);
+        }
+    }
 }
