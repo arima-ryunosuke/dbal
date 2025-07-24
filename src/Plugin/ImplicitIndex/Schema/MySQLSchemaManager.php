95841625c2910ea37114957107a527c139d14402
ryunosuke <ryunosuke.arima@gmail.com>
2025-07-24 19:34:57 +0900

    add ImplicitIndex plugin

---
+++ src/Plugin/ImplicitIndex/Schema/MySQLSchemaManager.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Doctrine\DBAL\Plugin\ImplicitIndex\Schema;
+
+use Doctrine\DBAL\Schema\SchemaConfig;
+
+trait MySQLSchemaManager
+{
+    /**
+     * @used-by \Doctrine\DBAL\Schema\MySQLSchemaManager::createSchemaConfig()
+     */
+    protected function interceptCreateSchemaConfigByImplicitIndex(SchemaConfig $schemaConfig): array
+    {
+        $schemaConfig->setExplicitForeignKeyIndexes(true);
+
+        return [];
+    }
+}
